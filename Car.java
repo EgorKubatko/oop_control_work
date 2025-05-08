c3e6b550751f10e71f6d60ba94617d6dd3f651f5
themo <kubatko.yehor@st.nkpk.op.edu.ua>
2025-05-08 21:48:02 +0300

    "Initial commit: added Car class"

---
+++ Car.java
@@ -0,0 +1,11 @@
+public class Car {private String brand;
+    private int year;
+
+    public void startEngine() {
+        System.out.println("Engine started!");
+    }
+
+    public void showInfo() {
+        System.out.println("Brand: " + brand + ", Year: " + year);
+    }
+}
